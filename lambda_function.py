727e35ad375dc6bf4012569d758b208ed6f3b345
SasmitaDevi <40880688+SasmitaDevi@users.noreply.github.com>
2024-09-03 01:05:44 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,7 @@ awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, servi
 print("Credentials access key:", credentials.access_key)
 print("Credentials secret key:", credentials.secret_key)
 
-host = 'https://search-mygoogle-74xgfxo3qbqg4mmm5zzt3a3uye.ap-northeast-1.es.amazonaws.com' # the OpenSearch Service domain, e.g. https://search-mydomain.us-west-1.es.amazonaws.com
+host = 'https://vpc-mysearchdomain-bnr5yhhw7qmfegva4u7cbvmg5a.us-east-1.es.amazonaws.com' # the OpenSearch Service domain, e.g. https://search-mydomain.us-west-1.es.amazonaws.com
 index = 'mygoogle'
 datatype = '_doc'
 #url = host + '/' + index + '/' + datatype
